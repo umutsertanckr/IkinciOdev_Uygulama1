f1a587c534a8dfa4f332a9b0052e95fba657af07
Umut Sertan ÇAKIR <umutsertanckr@gmail.com>
2020-11-23 12:11:42 +0300

    Proje dosyası ekle.

---
+++ IkinciOdev_Uygulama1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IkinciOdev_Uygulama1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double uzun, kısa;
+            Dikdörtgen hesap = new Dikdörtgen();
+            Console.Write("Uzun Kenarı Girin: ");
+            uzun = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Kısa Kenarı Girin: ");
+            kısa = Convert.ToDouble(Console.ReadLine());
+            hesap.alanhesapla(uzun, kısa);
+            hesap.cevrehesapla(uzun, kısa);
+        }
+    }
+    class Dikdörtgen
+    {
+        double h, b;
+        public void alanhesapla(double u, double k)
+        {
+            h = u * k;
+            Console.WriteLine("Alanı: " + h);
+        }
+        public void cevrehesapla(double u, double k)
+        {
+            b = (u + k) * 2;
+            Console.WriteLine("Çevresi: " + b);
+        }
+    }
+}
